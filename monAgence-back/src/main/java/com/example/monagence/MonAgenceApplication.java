36fd0db1f3801e9ff7a6e1713b84e3052230abef
julien1959 <dev.julien.f@gmail.com>
2021-10-13 12:24:22 +0200

    create back-end part

---
+++ monAgence-back/src/main/java/com/example/monagence/MonAgenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.monagence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonAgenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonAgenceApplication.class, args);
+    }
+
+}
---
+++ monAgence-back/src/test/java/com/example/monagence/MonAgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monagence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonAgenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
